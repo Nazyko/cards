45d23198a0f1650d53c05e606f935b0d33f7d97b
Nazyko <reypnazarovanazira@gmail.com>
2024-11-07 00:03:26 +0500

    Обновил App.js и index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/AboutPage"
+import Contact from './pages/Contact'
+import NotFoundPage from './pages/NotFoundPage'
+import  Layout from './components/Layout'
+import Singlepage from './pages/Singlepage'
 
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+const App = () => {
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route exact index element={<Home />}/>
+                    <Route path='/:id' element={<Singlepage />}/>
+                    <Route path='about' element={<About/>} />
+                    <Route path='contact' element={<Contact />} />
+                    <Route path='*' element={<NotFoundPage/>} />
+                </Route>
+            </Routes>
+        </>
+    )
 }
 
-export default App;
+export default App
